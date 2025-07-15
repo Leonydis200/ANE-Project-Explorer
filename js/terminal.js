3a1c18e783a5948b2e3246823d5561b009df51f8
Leonydis200 <juantempgreyling2@outlook.com>
2025-07-15 07:03:16 +0100

    Create terminal.js

---
+++ js/terminal.js
@@ -0,0 +1,8 @@
+<div class="cyber-terminal">
+  <div class="terminal-output"></div>
+  <div class="terminal-input">
+    <span class="prompt">ANE:~$ </span>
+    <input type="text" class="command-line" autofocus>
+  </div>
+</div>
+<script src="js/terminal.js"></script>
