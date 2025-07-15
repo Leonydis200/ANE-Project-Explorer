4dc7b100e8c1fa47388202859edc19f25315d971
Leonydis200 <juantempgreyling2@outlook.com>
2025-07-15 07:07:49 +0100

    Create audio.js

---
+++ js/audio.js
@@ -0,0 +1,4 @@
+<audio id="hover-sound" src="assets/sounds/hover.wav" preload="auto"></audio>
+<audio id="click-sound" src="assets/sounds/click.wav" preload="auto"></audio>
+<audio id="terminal-sound" src="assets/sounds/terminal.wav" preload="auto"></audio>
+<script src="js/audio.js"></script>
